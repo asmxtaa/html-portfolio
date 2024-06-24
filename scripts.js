2d8111f7b071276ed65b5726aadf0698e77c774f
Asmita Mandal <mandalasmita2017@gmail.co>
2024-06-24 17:02:26 +0530

    before_login_register(agni)

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchMarsWeather();
+    fetchAsteroids();
+    fetchGallery();
+});
+
+function fetchMarsWeather() {
+}
+
+function displayMarsWeather(data) {
+}
+
+function fetchAsteroids() {
+}
+
+function displayAsteroids(data) {
+}
+
+function fetchGallery() {
+}
+
+function displayGallery(data) {
+}
